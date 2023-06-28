b7266ab2dba038895d52094d15151cd1d86f9572
godlemon <78655414+godlemon@users.noreply.github.com>
2023-06-28 20:46:28 +0700

    heloo

--- DoAnThucTapFin/Controllers/ProductSPController.cs
+++ DoAnThucTapFin/Controllers/ProductSPController.cs
@@ -1,15 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DoAnThucTapFin.Controllers
 {
 	public class ProductSPController : Controller
 	{
-		public IActionResult Index()
+        private readonly ILogger<ProductSPController> _logger;
+        private readonly ApplicationDbContext dbContext;
+
+        public ProductSPController(ILogger<ProductSPController> logger, ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            this.dbContext = dbContext;
+        }
+		public async Task<IActionResult> DetailAsync (int productID)
 		{
-			return View();
-		}
-		public IActionResult Detail ()
-		{
+			var product = await dbContext.products.FindAsync(productID);
+
+			// Lấy giá trị của tagsname từ Tags.name
+			var tag = await dbContext.tags.FindAsync(product.TagId);
+			ViewBag.Product = product;
+			ViewBag.tagsname = tag?.Name;
 			return View();
 		}
 	}
